96bcf1610e95ce04641c67ccbec06b5f15017014
Ismail Tlemcani <ismail.tlemcani@gmail.com>
2023-04-02 14:38:06 +0200

    add section and remove unusued project section

---
+++ pages/tech-interviews.js
@@ -0,0 +1,15 @@
+export default function TechInterviews() {
+  return (
+    <>
+      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+        <div className="space-x-2 pt-6 pb-8 text-center md:space-y-5">
+          <h2 className="text-lg font-extrabold sm:text-4xl sm:leading-10 md:px-6 md:text-4xl">
+            Are you hiring a web developer ? I can do your technical interview for only 20€.
+          </h2>
+
+          <h3 className="text-lg">Contact me at itlem.hiring@gmail.com</h3>
+        </div>
+      </div>
+    </>
+  )
+}
