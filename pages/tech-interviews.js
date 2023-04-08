eb9a69844eb97589ab22ccc1f0f5fb6708362f55
Ismail Tlemcani <ismail.tlemcani@gmail.com>
2023-04-08 15:37:22 +0200

    change tech interview service message

--- pages/tech-interviews.js
+++ pages/tech-interviews.js
@@ -4,8 +4,11 @@ export default function TechInterviews() {
       <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
         <div className="space-x-2 pt-6 pb-8 text-center md:space-y-5">
           <h2 className="text-lg font-extrabold sm:text-4xl sm:leading-10 md:px-6 md:text-4xl">
-            Are you hiring a web developer ? I can do your technical interview for only 20€.
+            Are you hiring a fullstack developer ?
           </h2>
+          <h3 className="text-xl ">
+            Let me simplify your hiring process by taking care of your technical interviews.
+          </h3>
 
           <h3 className="text-lg">Contact me at itlem.hiring@gmail.com</h3>
         </div>
