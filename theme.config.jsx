84abc9299e1fe9f80b93d3f2eecbbee13cfcc6ba
Ismailtlem <ismail.tlemcani.91@gmail.com>
2023-07-30 17:30:21 +0200

    add nav links

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,4 +1,5 @@
 /* eslint sort-keys: error */
+/* eslint-disable import/no-anonymous-default-export */
 export default {
   components: {
     h1: ({ children }) => (
@@ -11,16 +12,6 @@ export default {
       </h1>
     ),
   },
-  head: ({ meta }) => (
-    <>
-      <title>Ismail Tlemcani - Software developer</title>
-      {meta.description && (
-        <meta name="description" content={meta.description} />
-      )}
-      {meta.tag && <meta name="keywords" content={meta.tag} />}
-      {meta.author && <meta name="author" content={meta.author} />}
-    </>
-  ),
   darkMode: true,
   dateFormatter: (date) => `Last updated at ${date.toDateString()}`,
   footer: (
@@ -40,4 +31,29 @@ export default {
       `}</style>
     </small>
   ),
+  head: ({ meta }) => (
+    <>
+      <title>Ismail Tlemcani - Software developer</title>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  navs: [
+    {
+      name: "RSS",
+      url: "/feed.xml",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/Ismailtlem/",
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/ismailtlemcani/",
+    }
+  ],
+  readMore: "Read More →",
 };
