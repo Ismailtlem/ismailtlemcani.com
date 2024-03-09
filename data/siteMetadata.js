11a507f7d6bb6046d5c20a72a2dde0d545551e1f
Ismailtlem <ismail.tlemcani.91@gmail.com>
2024-03-09 18:03:34 +0100

    change socialbanner image in sitemetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,13 +3,14 @@ const siteMetadata = {
   title: 'Ismail Tlemcani - Software Developer',
   author: 'Ismail Tlemcani',
   headerTitle: 'Ismail Tlemcani',
-  description: 'I help companies to provide value to their customers by using JavaScript and Python tools. I also love writing about what I am learning at the moment and working on exciting open source projects.',
+  description:
+    'I help companies to provide value to their customers by using JavaScript and Python tools. I also love writing about what I am learning at the moment and working on exciting open source projects.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://ismailtlemcani.com',
   siteRepo: 'https://github.com/Ismailtlem/ismailtlemcani.com',
-  siteLogo: '/static/images/avatar.png',
-  socialBanner: '/static/images/avatar.png',
+  siteLogo: '/static/favicons/favicon.ico',
+  socialBanner: '/static/favicons/favicon.ico',
   email: 'ismail@ismailtlemcani.com',
   github: 'https://github.com/Ismailtlem',
   twitter: 'https://twitter.com/Ismailtlem',
