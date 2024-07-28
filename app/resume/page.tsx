fb09eacd45f25c799e3df088f36f5721da64e2bf
Ismail Tlemcani <ismail@ismailtlemcani.com>
2024-07-29 00:43:25 +0200

    add alia

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,13 +1,13 @@
 import ResumeLayout from '@/layouts/ResumeLayout'
 import { genPageMetadata } from 'app/seo'
-import { allResumes } from 'contentlayer/generated'
+import { allResumes, Resume } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import { coreContent, MDXDocument } from 'pliny/utils/contentlayer'
+import { coreContent } from 'pliny/utils/contentlayer'
 
 export const metadata = genPageMetadata({ title: 'Resume' })
 
 export default function Page() {
-  const resume: MDXDocument = allResumes.find((p) => p.slug === 'resume')
+  const resume = allResumes.find((p) => p.slug === 'resume') as Resume
   const mainContent = coreContent(resume)
 
   return (
