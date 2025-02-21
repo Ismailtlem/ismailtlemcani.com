a8f677419e2a8582fbebc814fe132e161e5343d1
Ismail Tlemcani <ismail@ismailtlemcani.com>
2025-02-21 12:00:38 +0100

    add free option

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/resume', title: 'Resume' },
   { href: '/tags', title: 'Tags' },
-  { href: '/ama', title: 'Ask me anything' },
+  { href: '/ama', title: 'Ask me anything (Free)' },
 ];
 
 export default headerNavLinks;
