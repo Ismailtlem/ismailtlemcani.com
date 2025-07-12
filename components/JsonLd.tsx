ccd0f3306813c009aaaf46536a09c4f22856d77c
Ismail Tlemcani <ismail.tlemcani@gmail.com>
2025-07-12 10:19:52 +0200

    fix prettier

--- components/JsonLd.tsx
+++ components/JsonLd.tsx
@@ -1,35 +1,32 @@
 import React from 'react';
 
 interface JsonLdProps {
-    data: Record<string, any>;
+  data: Record<string, any>;
 }
 
 const JsonLd: React.FC<JsonLdProps> = ({ data }) => {
-    return (
-        <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-        />
-    );
+  return (
+    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }} />
+  );
 };
 
 export default JsonLd;
 
 export interface BreadcrumbItem {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 }
 
 export const BreadcrumbJsonLd: React.FC<{ items: BreadcrumbItem[] }> = ({ items }) => {
-    const data = {
-        '@context': 'https://schema.org',
-        '@type': 'BreadcrumbList',
-        itemListElement: items.map((item, idx) => ({
-            '@type': 'ListItem',
-            position: idx + 1,
-            name: item.name,
-            item: item.url,
-        })),
-    };
-    return <JsonLd data={data} />;
-}; 
+  const data = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: items.map((item, idx) => ({
+      '@type': 'ListItem',
+      position: idx + 1,
+      name: item.name,
+      item: item.url,
+    })),
+  };
+  return <JsonLd data={data} />;
+};
